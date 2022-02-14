fc6502f51126dac7e6989a8618e08ad5e5317a2c
Kamasu Paul <kamasupaul@gmail.com>
2022-02-14 20:03:29 +0300

    -- add incomes table

--- src-electron/database/database.js
+++ src-electron/database/database.js
@@ -20,4 +20,30 @@ database.schema.hasTable('students').then((exists) =>{
         })
     }
 })
+database.schema.hasTable('incomes').then((exists) => {
+  if(!exists) {
+    return database.schema.createTable('incomes',function(table) {
+      table.increments('id').primary();
+      table.integer('amount',100)
+      table.string('category',100)
+      table.string('date',100)
+      table.string('description',100)
+      table.string('reference_number',100)
+      table.timestamps()
+    })
+  }
+})
+// database.schema.hasTable('fees_structures').then((exists) => {
+//   if(!exists) {
+//     return database.schema.createTable('fees_structures',function(table) {
+//       table.increments('id').primary();
+//       table.string('student_id',100)
+//       table.string('category',100)
+//       table.string('date',100)
+//       table.string('description',100)
+//       table.integer('amount')
+//       table.timestamps()
+//     })
+//   }
+// })
 export default database;
